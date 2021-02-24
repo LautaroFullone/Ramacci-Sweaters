b4be8a9c1ca8533ff5ad2524fe4ebfd3759cdc39
rodriPerez19 <ropeque19@hotmail.com>
2021-02-23 23:15:49 -0300

    mail

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -6,12 +6,15 @@ use Illuminate\Http\Request;
 use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\PHPMailer;
 
+use Illuminate\Support\Facades\Mail;
+use App\Mail\MensajeEnviado;
+
 class MailController extends Controller
 {
     public function send(Request $request)
     {
         // Instantiation and passing `true` enables exceptions
-        $mail = new PHPMailer(true);
+       /* $mail = new PHPMailer(true);
 
         try {
             //Server settings
@@ -39,11 +42,11 @@ class MailController extends Controller
         } catch (Exception $e) {
             echo "Hubo un error al enviar el mail:  {$mail->ErrorInfo}";
         } finally {
-            $id = $request->input('id');
-            // Mail::to('ropeque19@hotmail.com')->send(new MensajeEnviado($mensaje));
+            $id = $request->input('id');*/
 
+            Mail::to('ropeque19@hotmail.com')->send(new MensajeEnviado($request->all()));
             return redirect()->back();
         }
 
-    }
 }
+
